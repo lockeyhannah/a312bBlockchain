5c649e4c0d9686fd8e29a86d59e7991937855a52
Hannah Lockey <hannah97@live.dk>
2018-04-17 10:17:04 +0200

    Initial commit

---
+++ src/Block.java
@@ -0,0 +1,36 @@
+import java.util.Date;
+
+public class Block {
+
+    public String hash; //hashen fra nuværende block
+    public String previousHash; //hashen fra en tidligere block
+    private String data; //dataen fra blocken
+    private long timeStamp;
+    private int nonce;
+
+    public Block(String data, String previousHash) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+        this.hash = calculateHash();
+
+    }
+
+    public String calculateHash() {
+        String calculatedHash = StringUtil.applySha256(
+                previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + data);
+        return calculatedHash;
+    }
+
+    public void mineBlock(int difficulty){
+        //Laver en String med difficulty 0:
+        String target = new String(new char[difficulty]).replace('\0', '0');
+
+        while (!hash.substring(0, difficulty).equals(target)) {
+            nonce++;
+            hash = calculateHash();
+        }
+        System.out.println("Blocken er lavet: " + hash);
+    }
+
+}
---
+++ src/NoobChain.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+public class NoobChain {
+
+    public static ArrayList<Block> blockchain = new ArrayList<>();
+
+    //Hvor mange nuller der skal være
+    public static int difficulty = 3;
+
+    public static void main(String[] args) {
+
+        String info = new String("hej");
+
+        blockchain.add(new Block("first block", "0"));
+        System.out.println("Igang med at lave block 1...");
+        blockchain.get(0).mineBlock(difficulty);
+
+        for (int i = 1; i < 5; i++){
+            blockchain.add(new Block(info, blockchain.get(blockchain.size()-1).hash));
+            System.out.println("Igang med at lave block " + (i+1) + "..." );
+            blockchain.get(i).mineBlock(difficulty);
+        }
+
+        System.out.println("\nBlockchainen er ok: " + isChainValid() + "\n");
+
+        for (int i = 0; i < blockchain.size(); i++){
+            System.out.println("Hash for block " + i + ": " + blockchain.get(i).hash);
+        }
+    }
+
+    //Tjekker om blockchainens data matcher hinanden
+    public static Boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+
+        //Laver en ny String med difficulty 0
+        String hashTarget = new String(new char[difficulty]).replace('\0', '0');
+
+        //Dette forloop tjekker de tre exceptions for hver block.
+        for (int i = 1; i < blockchain.size(); i++){
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i - 1);
+
+            if(!currentBlock.hash.equals(currentBlock.calculateHash())){
+                System.out.println("Nuværende Hashes er ikke ens.");
+                return false;
+            }
+
+            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+                System.out.println("Tidligere Hashes er ikke ens.");
+                return false;
+            }
+
+            if (!currentBlock.hash.substring(0, difficulty).equals(hashTarget)){
+                System.out.println("Denne block er ikke lavet.");
+                return false;
+            }
+        }
+        return true;
+    }
+}
