b04c6c7b81f25411d16e9a58b0935eb7471b1dc7
Hannah <hannah97@live.dk>
2018-04-18 13:09:49 +0200

    igang med at lave tests

--- test/blockTest.java
+++ test/blockTest.java
@@ -1,2 +1,31 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class blockTest {
+
+    Block block1 = new Block("0","0");
+    Block block2 = new Block("0","0");
+
+    @BeforeEach
+    public void beforeTests(){
+        block1 = new Block("hej med dig", "0");
+        block2 = new Block("hoohua", block1.hash);
+    }
+
+
+    @Test
+    public void blockTest01(){
+        assertEquals(block2.hash, block2.calculateHash());
+    }
+
+    @Test
+    public void blockTest02(){
+        assertEquals(block2.previousHash, block1.hash);
+    }
+
+    @Test
+    public void blockTest03(){
+
+    }
 }
--- test/noobChainTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-
-public class noobChainTest {
-
-    @Test
-    public void blockTest1(){
-
-    }
-}
