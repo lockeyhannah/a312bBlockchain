33960dcbf05d458d23eccc4f8672ce1a33e158d4
Hannah <hannah97@live.dk>
2018-04-19 13:00:05 +0200

    Merge branch 'master' of C:\Users\Hanna\IdeaProjects\Blockchain with conflicts.

--- src/Block.java
+++ src/Block.java
@@ -1,5 +1,4 @@
 import block.Data;
-import block.Header;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
@@ -9,22 +8,17 @@ import java.util.Date;
 public class Block {
 
     //hej
-
-    //Data
-    //Datapoints[] - getStringValue(),
-    //Smart contract, valuta,  nøgler
-    //
-
     private Data data; //dataen fra blocken
-    private Header header;
+
 
     public Block(String previousHash) {
         data = new Data();
-        header = new Header();
+        this.data.previousHash = previousHash;
         this.data.timeStamp = new Date().getTime();
+        this.data.hash = calculateHash(data.getData().getBytes(), BigInteger.ONE.toByteArray());
     }
 
-    public static byte[] calculateHash(byte[] input, byte[] nonce) {
+    public byte[] calculateHash(byte[] input, byte[] nonce) {
         try{
             MessageDigest msgDigest = MessageDigest.getInstance("SHA-256");
             msgDigest.update(input);
@@ -37,25 +31,29 @@ public class Block {
         return null;
     }
 
-    /* */
     public void mineBlock(BigInteger target){
         BigInteger nonce = new BigInteger("0");
-
-        //Calculate the hash value with the default nonce and convert to an integer value
-        byte[] hash = calculateHash(data.getDataString().getBytes(), nonce.toByteArray());
+        byte[] hash = calculateHash(data.getData().getBytes(), nonce.toByteArray());
         BigInteger hashInteger = new BigInteger(1, hash);
 
-        // Recalculate hash with new nonce values until the hash is below the target value
         while(hashInteger.compareTo(target) > 0){
             nonce = nonce.add(BigInteger.ONE);
-            hash = calculateHash(data.getDataString().getBytes(), nonce.toByteArray());
+            hash = calculateHash(data.getData().getBytes(), nonce.toByteArray());
             hashInteger = new BigInteger(1, hash);
         }
 
-        // TODO: 19-04-2018 : save hash and nonce
+        StringBuilder hexString = new StringBuilder();
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xFF & hash[i]);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
 
-
-
+        System.out.println("Nonce : " + nonce.toString());
+        System.out.println("Hash (Decimal) : " + hashInteger.toString());
+        System.out.println("Hash (Hex) : " + hexString.toString());
     }
 
 
