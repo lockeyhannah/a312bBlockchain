4540b8c1aa33d59b3f6e5d55e4c8f5bbc9ac59e6
Tobias <nielstns@gmail.com>
2018-04-27 14:10:21 +0200

    Fucked up some shite. Guess where.

--- src/blockchain/ledger_file/LedgerReader.java
+++ src/blockchain/ledger_file/LedgerReader.java
@@ -20,56 +20,12 @@ public class LedgerReader {
 
     // Reads and returns the block with the given id
     public Block readBlock(long blockID){
-        byte[] getBytes;
-        byte[] target = longToBytes(blockID);
-        int x = 0;
-        try {
-            File file = new File(ledgerFilePath.toString());
-            getBytes = new byte[4];
-            InputStream is = new FileInputStream(file);
-            while(is.read(getBytes, x, 4) != -1) {
-                if (Arrays.compare(getBytes,target) == 0) {
-                    byte[] tempPrevHash = new byte[32];
-                    is.read(tempPrevHash, x+4, 32);
-                    byte[] tempDataHash = new byte[32];
-                    is.read(tempDataHash, x+36, 32);
-                    byte[] tempNonce = new byte[32];
-                    is.read(tempNonce, x+68, 32);
-                    byte[] tempTarget = new byte[32];
-                    is.read(tempTarget, x+100, 32);
-                    byte[] tempTimestamp = new byte[8];
-                    is.read(tempTimestamp, x+132, 8);
-
-                    Header newHeader = new Header(bytesToLong(getBytes),tempPrevHash,tempDataHash,tempNonce,tempTarget,tempTimestamp);
-
-                    byte[] newbyte = new byte[64];
-                    is.read(newbyte, x+140, 4);//todo: Insæt det der data længde
-
-                    Data newData = newbyte.byteToData; //Todo: Temporary.
-
-                    Block newBlock = new Block(newData, newHeader);
-                    is.close();
-                    return newBlock;
-                    //Todo: compact this this shit.
-                } else {
-                    //ToDo: Find ud af hvor mange byte data som standard er.
-                }
-            }
-                is.close();
-            System.out.println("Block not found. Returning blank block.");
-            return new Block(null,null);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         return null;
     }
 
     // TODO: 24-04-2018 : make this shit
     //Returns the hash of the previous blocks header
     public byte[] getPreviousHeaderHash(){
-
         return null;
     }
 
