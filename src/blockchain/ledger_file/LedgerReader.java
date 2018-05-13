c8f17824ae56fa3c6e3e3420f7e98382f20c64fc
Hannah <hannah97@live.dk>
2018-05-13 20:16:57 +0200

    testies

--- src/blockchain/ledger_file/LedgerReader.java
+++ src/blockchain/ledger_file/LedgerReader.java
@@ -92,6 +92,7 @@ public class LedgerReader {
     private byte[] readBytes(int byteCount){
         try (BufferedInputStream bis = new BufferedInputStream(Files.newInputStream(ledgerFilePath))) {
             byte[] bytes = new byte[byteCount];
+            // TODO: 13-05-2018 hva sker der her
             bis.read(bytes, 0, 8);
             return bytes;
         } catch (IOException e) {
--- test/blockchain/ledger_file/LedgerReaderTest.java
+++ test/blockchain/ledger_file/LedgerReaderTest.java
@@ -1,5 +1,6 @@
 package blockchain.ledger_file;
 
+import blockchain.block.Block;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,23 +12,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LedgerReaderTest {
 
-
+    Path path1, path2;
+    Block block;
 
     @BeforeEach
     public void ledgerReaderTest00(){
-
+        path1 = Paths.get("reader_test.txt");
+        path2 = Paths.get("chain_test.txt");
     }
 
     // TODO: 11-05-2018 reeeeeee
 
     @Test
     public void ledgerReaderTest01(){
-        Path path = Paths.get("chain_test.txt");
         try {
-            new LedgerReader(path);
+            new LedgerReader(path1);
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            System.out.println("File not found.");
         }
     }
 
+    @Test
+    public void ledgerReaderTest02(){
+
+        try {
+            block = new LedgerReader(path2).getFirstBlock();
+        } catch (FileNotFoundException e) {
+            System.out.println("couldnt get block");
+        }
+
+        System.out.println(block.toString());
+    }
 }
